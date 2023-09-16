ca2323991e8c2cf327c5e4ed830755f8a26c7fdc
Fang Jin <windmaomao@gmail.com>
2023-09-16 13:49:37 -0400

    add one button

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
+#include <JC_Button.h>
 
 #define PIN_LED LED_BUILTIN
-#define PIN_BUTTON 4
+#define PIN_KEY_1 (4)
+
+Button key1(PIN_KEY_1);
 
 void setup()
 {
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
   Serial.begin(9600);
+  pinMode(PIN_LED, OUTPUT);
+  key1.begin();
 }
 
 void loop()
 {
-  digitalWrite(PIN_LED, LOW);
-  Serial.println(digitalRead(PIN_BUTTON));
+  key1.read();
+  if (key1.wasPressed())
+  {
+    digitalWrite(PIN_LED, HIGH);
+  }
+  if (key1.wasReleased())
+  {
+    digitalWrite(PIN_LED, LOW);
+  }
 }
