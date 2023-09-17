0201b4edeeb5e0060219bfadb8c560ac3a9c4291
Fang Jin <windmaomao@gmail.com>
2023-09-17 19:52:50 -0400

    add rotary support for esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <AiEsp32RotaryEncoder.h>
 #include <Arduino.h>
 #include <Button2.h>
 
@@ -8,6 +9,7 @@
 
 Button2 key;
 volatile int count = 0;
+AiEsp32RotaryEncoder rotary = AiEsp32RotaryEncoder(PIN_ROTARY_LEFT, PIN_ROTARY_RIGHT);
 
 IRAM_ATTR void onPress(Button2 &btn)
 {
@@ -21,6 +23,11 @@ IRAM_ATTR void onRelease(Button2 &btn)
   Serial.println(count);
 }
 
+void IRAM_ATTR readEncoderISR()
+{
+  rotary.readEncoder_ISR();
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -28,10 +35,18 @@ void setup()
   key.begin(PIN_KEY);
   key.setPressedHandler(onPress);
   key.setReleasedHandler(onRelease);
+  pinMode(PIN_ROTARY_LEFT, INPUT_PULLUP);
+  pinMode(PIN_ROTARY_RIGHT, INPUT_PULLUP);
+  rotary.begin();
+  rotary.setup(readEncoderISR);
+  rotary.areEncoderPinsPulldownforEsp32 = false;
 }
 
 void loop()
 {
   key.loop();
-  delay(50);
+  if (rotary.encoderChanged())
+  {
+    Serial.println(rotary.readEncoder());
+  }
 }
